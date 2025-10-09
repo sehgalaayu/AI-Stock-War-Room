97f7fb86cb6a56933ed60c11d4b5e5afa2bc1f7d
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-10-09 22:06:29 +0530

    created auth route

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,43 @@
+import prisma from "../lib/prisma";
+import { hashPwd, comparePwd, generateToken } from "../lib/auth";
+import { registerSchema, loginSchema } from "../lib/validation";
+import express, { Request, Response } from "express";
+import { stripTypeScriptTypes } from "module";
+
+const router = express.Router();
+
+//POST  - api/auth/register
+router.post("/register", async (req: Request, res: Response) => {
+  try {
+    const validatedData = registerSchema.parse(req.body);
+    const { name, email, password } = validatedData;
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      res.status(400).json({
+        error: "User already exists",
+      });
+    }
+    const hashed = await hashPwd(password);
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashed,
+      },
+    });
+    const token = generateToken(user.id);
+    res.status(201).json({
+      message: "User created successfully",
+      token,
+      user: { id: user.id, name: user.name, email: user.email },
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Server error",
+    });
+    console.log("User could not be created : ", error);
+  }
+});
